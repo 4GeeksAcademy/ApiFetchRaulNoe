eee2b78f9048daeccb8b79c711803333ca2c71fb
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-08-21 19:34:53 +0000

    Trying to make CURD

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,28 +1,29 @@
-import { useEffect, useState } from 'react'
+import { useState } from "react"
+import useGetData from './hooks/useFetch';
+import { handleDeleteData, handlePutData } from './services/modifyData';
 // import LisOfNames from './components/ListOfNames'
 
 
 const App = () => {
-  const [users, setUsers] = useState([])
-  
-  useEffect(() => {
-    fetch("https://www.swapi.tech/api/people")
-    .then(respuesta => respuesta.json())
-    .then(respuesta => {
-      setUsers(respuesta.results);
-    })
-    .catch(error => console.log(error));
-
-  }, [])
-  
+  const data = useGetData("https://www.swapi.tech/api/people")
+  const [usersInfo, setUsersInfo] = useState(data)
+   
   return (
-    <ul>
-      {users.map(people => {
-        return (
-          <li key={people.uid}><a href={people.url}>{people.name}</a></li>
-        )
-      })}
-    </ul>)
+    <>
+      <ul>
+        {usersInfo.map(people => {
+          return (
+            <li key={people.uid}>
+            <a href={people.url}>{people.name}</a>
+            <button onClick={()=>handleDeleteData(people.uid)}>Delete</button>
+            </li>
+          )
+        })}
+      </ul>
+      <button onClick={()=>handlePutData(data)}>Put Data</button>
+      
+    </>
+    )
 };
 
-export default App
+export default App;
