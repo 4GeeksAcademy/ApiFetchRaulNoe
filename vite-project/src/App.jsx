3780f1e9d48e236fc5c975229c1077e2d1bfc4ae
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-08-23 16:13:16 +0000

    fixing put/delete

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,29 +1,43 @@
-import { useState } from "react"
+import React, { useState, useEffect } from "react";
 import useGetData from './hooks/useFetch';
 import { handleDeleteData, handlePutData } from './services/modifyData';
-// import LisOfNames from './components/ListOfNames'
-
 
 const App = () => {
-  const data = useGetData("https://www.swapi.tech/api/people")
-  const [usersInfo, setUsersInfo] = useState(data)
-   
+  const data = "https://www.swapi.tech/api/people";
+  const fetchedData = useGetData(data);
+
+  const [loading, setLoading] = useState(true);
+  const [usersInfo, setUsersInfo] = useState([]);
+
+  useEffect(() => {
+    setUsersInfo(fetchedData);
+    setLoading(false);
+  }, [fetchedData]);
+
+  const handleDelete = (uid) => {
+    console.log('Before delete:', usersInfo);
+    handleDeleteData(uid);
+    const updatedUsersInfo = usersInfo.filter(people => people.uid !== uid);
+    console.log('After delete:', updatedUsersInfo);
+    setUsersInfo(updatedUsersInfo);
+  };
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <ul>
-        {usersInfo.map(people => {
-          return (
-            <li key={people.uid}>
+        {usersInfo.map(people => (
+          <li key={people.uid}>
             <a href={people.url}>{people.name}</a>
-            <button onClick={()=>handleDeleteData(people.uid)}>Delete</button>
-            </li>
-          )
-        })}
+            <button onClick={() => handleDelete(people.uid)}>Delete</button>
+          </li>
+        ))}
       </ul>
-      <button onClick={()=>handlePutData(data)}>Put Data</button>
-      
+      <button onClick={() => handlePutData(data)}>Put Data</button>
     </>
-    )
+  );
 };
 
 export default App;
--- vite-project/src/components/ListOfNames.jsx
+++
@@ -1,8 +0,0 @@
-const LisOfNames = ({children})=>{
-    return(
-        <li>{children}</li>
-    )
-
-}
-
-export default LisOfNames;
